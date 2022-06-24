4af09cf2db7cfc8d99836b37a20127b31f536018
GermanChrystan-MeLi <german.chrystan@mercadolibre.com>
2022-06-24 17:33:25 -0300

    Main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"database/sql"
+	"os"
+
+	"github.com/GermanChrystan-MeLi/team_manager/cmd/server/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 	// name, _ := names.CreateFullNameByNationality(constants.Brazil)
 	// fmt.Println(name)
+	_ = godotenv.Load()
+	db, _ := sql.Open("mysql", os.Getenv("DB_URL"))
+
+	r := gin.Default()
+	router := routes.NewRouter(r, db)
+	router.MapRoutes()
+
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
 }
