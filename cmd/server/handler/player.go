5de9e412fc440f842a772f62ae7dd9a4b0467e4c
GermanChrystan-MeLi <german.chrystan@mercadolibre.com>
2022-06-24 16:52:40 -0300

    Player Handler

---
+++ cmd/server/handler/player.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/GermanChrystan-MeLi/team_manager/internal/player"
+	"github.com/GermanChrystan-MeLi/team_manager/pkg/web"
+	"github.com/gin-gonic/gin"
+)
+
+//=================================================================================//
+type Player struct {
+	playerService player.PlayerService
+}
+
+//=================================================================================//
+func NewPlayer(ps player.PlayerService) *Player {
+	return &Player{
+		playerService: ps,
+	}
+}
+
+//=================================================================================//
+func (p *Player) GetPlayerById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.Background()
+		playerID := c.Request.Header.Get("player_id")
+		if playerID == "" {
+			web.Error(c, 400, "missing player id")
+			return
+		}
+		player, err := p.playerService.GetPlayerById(ctx, playerID)
+		if err != nil {
+			web.Error(c, 404, err.Error())
+			return
+		}
+		web.Success(c, 200, player)
+	}
+}
+
+//=================================================================================//
